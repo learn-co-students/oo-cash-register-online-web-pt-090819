667330cebd00b8d89c5d8726053b90eeeba907a4
Chaya Deaver <chayadeaver@gmail.com>
2019-09-23 21:21:13 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
+require 'pry'
+class CashRegister
+attr_accessor :total, :discount
 
+def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+end
+
+def add_item(title, price, quantity = 1)
+@total = quantity * price
+@total
+end
+
+
+
+end
